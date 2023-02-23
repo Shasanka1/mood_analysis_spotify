ed55c518617ef357ad4a0cbe05538afc14124880
Shasanka <snudurupati@ucsd.edu>
2023-02-22 16:28:53 -0800

    spotify analysis

---
+++ config.py
@@ -0,0 +1,13 @@
+"""
+Created on Wed Jan 29 10:29:55 2020
+
+@author: vlad
+"""
+
+#insert local parameters
+
+username =  # 'your-spotify-username'
+client_id =  #'your-client-id'
+client_secret =  #'your-client-secret'
+redirect_uri =  #'your-redirect-uri'
+scope = 'user-read-recently-played'
